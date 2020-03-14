31fac80f25eff61d51fa4c5a17868b08479564eb
Justin Casso <justincasso1@gmail.com>
2020-03-14 13:25:59 -0700

    Hack Phase  - Initial Dockerization commit

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,18 @@ const index = require('./routes/index.route.js');
 const user = require('./routes/user.route.js');
 const motd = require('./routes/motd.route.js');
 
+var motdModel = require('./models/motd.model.js');
+
 const app = express();
 const port = 3000;
 const ip = "0.0.0.0";
+
+mongoose.connect('mongodb://mongo:27017/app', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connection to `app` database successful.');
+});
 
 app.use(cors())
 app.use('/', index)
