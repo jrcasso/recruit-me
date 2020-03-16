5e774b673054ff5e95227caa814167d1874b6bb8
Justin Casso <justincasso1@gmail.com>
2020-03-16 00:43:51 -0700

    Hack Phase  - Add login, register, reset, home, and welcome components  - Add first-pass look/feel  - Add assets and skeleton code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from '@modules/material.module';
+
+import { WelcomeComponent } from '@components/welcome/welcome.component';
+import { HomeComponent } from '@components/home/home.component';
+import { LoginComponent } from '@components/login/login.component';
+import { RegisterComponent } from '@components/register/register.component';
+import { ResetComponent } from '@components/reset/reset.component';
+
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'reset', component: ResetComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  { path: '**', redirectTo: '/welcome', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
   ],
