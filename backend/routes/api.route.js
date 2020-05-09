61b48385b7eb1f48653345e3296d76d42b4db3a8
Justin Casso <jcasso@agari.com>
2020-05-09 02:48:50 -0700

    Temporary for transfer

--- backend/routes/api.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const motdRoute = require('./motd.route.js');
-const { check } = require('express-validator');
-
-router.use('/motd', motdRoute);
-
-module.exports = router;
