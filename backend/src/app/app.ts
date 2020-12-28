2b0b554e40667be4a04eeafd8f159c6eaad4b692
Justin Casso <justincasso1@gmail.com>
2020-12-27 18:56:43 -0800

    Linting

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -16,22 +16,23 @@ import * as Express from 'express';
 import * as Cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
-import { ApiRouter }  from "./routes/api.router"
+import { ApiRouter } from './routes/api.router';
 
 class App {
-  app: any;
-  api_router: any;
   constructor() {
-    this.initialize_database_connection().then(() => {
+    this.connect().then(() => {
       this.app = Express();
-      this.api_router = new ApiRouter()
+      this.apiRouter = new ApiRouter();
       this.configure_middleware();
       this.run();
-    })
+    });
   }
 
-  private async initialize_database_connection(host="mongo", port=27017, database="app"): Promise<void> {
-    let endpoint = `mongodb://${host}:${port}/${database}`;
+  public app: any;
+  public apiRouter: ApiRouter;
+
+  private async connect(host= 'mongo', port= 27017, database= 'app'): Promise<void> {
+    const endpoint = `mongodb://${host}:${port}/${database}`;
     console.log(`Connecting to ${endpoint}`);
     await mongoose.connect(`mongodb://${host}:${port}/${database}`, {
       useNewUrlParser: true,
@@ -43,60 +44,18 @@ class App {
   }
 
   private configure_middleware(): void {
-    this.app.use(Cors())
+    this.app.use(Cors());
     this.app.use(bodyParser.urlencoded({extended: true}));
     this.app.use(bodyParser.json());
-    this.app.use('/api/v1', this.api_router.router)
+    this.app.use('/api/v1', this.apiRouter.router);
   }
 
-  private run(host="0.0.0.0", port=3000): void {
+  private run(host= '0.0.0.0', port= 3000): void {
     this.app.listen( port, host, () => {
       console.log( `server started at http://${ host }:${ port }` );
     });
   }
 }
 
-new App()
-// class App {
-//   public express: express.Application;
-//   private port = 3000;
-//   private host = "0.0.0.0";
-
-//   constructor() {
-//     this.express = express();
-//     this.setupDb();
-//     this.configure_middleware();
-//     this.listen();
-//   }
-
-//   private configure_middleware(): void {
-//     this.express.use(Cors())
-//     this.express.use(bodyParser.urlencoded({extended: true}));
-//     this.express.use(bodyParser.json());
-//     this.express.use('/api/v1', route.router)
-//   }
-
-//   private listen(host = null, port = null): void {
-//     this.express.listen(
-//       port == null ? this.port : port,
-//       host == null ? this.host : host,
-//       () => {
-//         console.log( `server started at http://${ host }:${ port }` );
-//       }
-//     );
-//   }
-
-//   private setupDb(): void {
-//     Mongoose.connect('mongodb://mongo:27017/app', {
-//       useNewUrlParser: true,
-//       useFindAndModify: false,
-//       useUnifiedTopology: true,
-//       bufferCommands: false,
-//     });
-//     var db = Mongoose.connection;
-//     db.on("error", console.error.bind(console, "MongoDB Connection error"));
-//     db.once('open', () => {
-//       console.log('Connection to `app` database successful.');
-//     });
-//   }
-// }
+// tslint:disable-next-line:no-unused-expression
+new App();
