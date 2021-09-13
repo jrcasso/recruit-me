e33d8a5ad1c375aaad86639748bbb9b27ccf51f8
Justin Casso <justincasso1@gmail.com>
2021-09-12 21:15:46 -0700

    Add spec tests for authentication endpoint

--- backend/e2e/controllers/auth.spec.js
+++ backend/e2e/controllers/auth.spec.js
@@ -3,16 +3,26 @@ const request = require('supertest');
 const RandomHelper = require('../helpers/random.helper');
 const UserHelper = require('../helpers/user.helper');
 const AuthHelper = require('../helpers/auth.helper');
+const decode = require('jose/util/base64url')
 
 describe('API endpoint for auth', function() {
   beforeAll(async function() {
     this.authHelper = new AuthHelper();
     this.userHelper = new UserHelper();
+    this.textDecoder = new TextDecoder()
     this.localRequest = request('http://express:3000');
     this.apiPath = '/api/v1';
     this.email = 'jdoe@test.justinshipscode.com';
     this.password = 'foob!@arBaz1023';
     this.passwordHash = '$2b$10$ftteQVXjsPVLA5ZRU/rRCO60B0wMjh6q3vlNdOpveaEnAzYuXmeBy';
+    this.firstname = 'John';
+    this.lastname = 'Doe';
+    this.user = {
+      email: this.email,
+      password: this.passwordHash,
+      firstname: this.firstname,
+      lastname: this.lastname
+    };
     this.authRequest = {
       email: this.email,
       password: this.password,
@@ -21,56 +31,94 @@ describe('API endpoint for auth', function() {
     await Promise.all([
       this.authHelper.connect(),
       this.userHelper.connect()
-    ]).then(() => {
-      this.authHelper.removeAll().catch((err) => console.error(err));
-      this.userHelper.removeAll().catch((err) => console.error(err));
-
-      this.userHelper.create({
-        email: this.email,
-        password: this.passwordHash,
-        firstname: 'John',
-        lastname: 'Doe',
-        verified: true,
-        active: true
-      });
-    });
+    ]);
 
   });
 
+  beforeEach(async function() {
+    await this.authHelper.removeAll().catch((err) => console.error(err));
+    await this.userHelper.removeAll().catch((err) => console.error(err));
+  });
+
   describe('with POST /auth/', function() {
-    it('creates a valid auth', function(done) {
+    it('creates a valid auth', async function(done) {
+      await this.userHelper.create({ ...this.user, ...{ active : true, verified : true } });
       this.localRequest
         .post(`${this.apiPath}/auth`)
         .send(this.authRequest)
         .expect(201)
+        .expect('Content-Type', /json/)
         .expect((res) => {
-          expect(res.body.user_id).toBe(this.email);
-          expect(res.body.token).toBe(this.firstname);
-          expect(res.body.created).toBe(this.lastname);
-          expect(res.body.expiry).toBe(null);
+          sections = []
+          expect(res.body.token.length).toBeGreaterThan(0);
+          res.body.token.split('.').forEach((section, index) => {
+            if (index == 0 || index == 1) {
+              sections.push(JSON.parse(this.textDecoder.decode(decode.decode(section))))
+            } else {
+              // The EC256 signature is not a JSON
+              sections.push(section)
+            }
+          })
+
+          // Mongo object IDs are 24 characters in length
+          // The creation of the authentication entry should have happened recently
+          // The expiry of the authentication entry should be greater than now but less than 4 weeks
+          expect(res.body._id.length).toBe(24);
+          expect(res.body.user_id.length).toBe(24);
+          expect(Date.parse(res.body.created)).toBeLessThan(Date.now());
+          expect(Date.parse(res.body.created)).toBeGreaterThan(Date.now() - 5000);
+          expect(Date.parse(res.body.expiry)).toBeGreaterThan(Date.now());
+          expect(Date.parse(res.body.expiry)).toBeLessThan(Date.now() + (31 * 24 * 60 * 60 * 1000));
+
+          // The token algorithm field indicates EC256
+          // The token claims field has a recent iat
+          // The token claims field has a future exp
+          // The token claims field has a defined iss
+          // The token claims field has a defined aud
+          // The token signature field is defined
+          expect(sections[0].alg).toBe('ES256')
+          expect(sections[1].iat).toBeLessThan(Date.now() / 1000)
+          expect(sections[1].exp).toBeGreaterThan(Date.now() / 1000)
+          expect(sections[1].iss.length).toBeGreaterThan(0)
+          expect(sections[1].aud.length).toBeGreaterThan(0)
+          expect(sections[2].length).toBeGreaterThan(0)
         })
         .end((err, res) => {
-          console.log(res.body);
           if (err) {return done(err);}
           done();
         });
     });
 
-    it('refuses to create a token for unverified accounts', function(done) {
-      // Copy and override a valid payload with an invalid email
-      const invalidEmails = [null, 0, '', false];
-      invalidEmails.forEach(email => {
-        const auth = { ...this.authRequest, ...{ email } };
-        this.localRequest
-          .post(`${this.apiPath}/auth`)
-          .send(auth)
-          .expect(422, { errors: [{
-            value: email,
-            msg: 'Email cannot be empty.',
-            param: 'email',
-            location: 'body'
-          }]}, done);
-      });
+    it('refuses to create a token for unverified accounts', async function(done) {
+      // Copy and override a valid payload with an unverified user
+      await this.userHelper.create({ ...this.user, ...{ active : true, verified : false } });
+      this.localRequest
+        .post(`${this.apiPath}/auth`)
+        .send(this.authRequest)
+        .expect(401)
+        .expect((res) => {
+          expect(res.body.message).toBe('User has not been verified')
+        })
+        .end((err) => {
+          if (err) {return done(err);}
+          done();
+        });
+    });
+
+    it('refuses to create a token for inactive accounts', async function(done) {
+      // Copy and override a valid payload with an inactive user
+      await this.userHelper.create({ ...this.user, ...{ active : false, verified : true } });
+      this.localRequest
+        .post(`${this.apiPath}/auth`)
+        .send(this.authRequest)
+        .expect(401)
+        .expect((res) => {
+          expect(res.body.message).toBe('User is inactive')
+        })
+        .end((err) => {
+          if (err) {return done(err);}
+          done();
+        });
     });
 
     it('refuses to create a auth with an empty email', function(done) {
@@ -83,7 +131,7 @@ describe('API endpoint for auth', function() {
           .send(auth)
           .expect(422, { errors: [{
             value: email,
-            msg: 'Email cannot be empty.',
+            msg: 'Email cannot be empty',
             param: 'email',
             location: 'body'
           }]}, done);
@@ -99,7 +147,7 @@ describe('API endpoint for auth', function() {
         .send(invalidUser)
         .expect(422, { errors: [{
           value: randomString,
-          msg: 'Email provided must be valid.',
+          msg: 'Email provided must be valid',
           param: 'email',
           location: 'body'
         }]}, done);
@@ -115,7 +163,7 @@ describe('API endpoint for auth', function() {
           .send(auth)
           .expect(422, { errors: [{
             value: password,
-            msg: 'Password cannot be empty.',
+            msg: 'Invalid credentials',
             param: 'password',
             location: 'body'
           }]}, done);
@@ -124,6 +172,23 @@ describe('API endpoint for auth', function() {
   });
 
   describe('with DELETE /auth/:id', function() {
+    it('rejects to delete for empty ObjectIds', function(done) {
+      const invalidIds = [ 'null', 0, '', false ];
+
+      invalidIds.forEach(id => {
+        this.localRequest
+          .delete(`${this.apiPath}/auth/${id}`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(400, { errors: [{
+            value: id.toString(),
+            msg: 'ID parameter must be valid',
+            param: 'id',
+            location: 'params'
+          }]}, done);
+      });
+    });
+
     it('rejects to delete for invalid ObjectIds', function(done) {
       this.localRequest
         .delete(`${this.apiPath}/auth/99999`)
@@ -131,7 +196,7 @@ describe('API endpoint for auth', function() {
         .expect('Content-Type', /json/)
         .expect(400, { errors: [{
           value: '99999',
-          msg: 'ID parameter must be valid.',
+          msg: 'ID parameter must be valid',
           param: 'id',
           location: 'params'
         }]}, done);
